98b9031bc4d5c87748a28db98e0597bdd215cd4a
Vivek-Jayaswal <141914455+Vivek-Jayaswal@users.noreply.github.com>
2024-03-06 09:55:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+
+const name = document.getElementById("name");
+const age = document.getElementById("age");
+const button = document.getElementById("btn");
+
+name.addEventListener("input",(e)=>{
+	const nameVal = e.target.value;
+})
+age.addEventListener("input",(e)=>{
+	const ageVal = e.target.value;
+})
+button.addEventListener("click",()=>{
+	
+})
+
+function 
+
+
+
