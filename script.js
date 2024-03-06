68d5c0016773db89bc14b1bcbda997fff1ef0bd2
Vivek-Jayaswal <141914455+Vivek-Jayaswal@users.noreply.github.com>
2024-03-06 11:43:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
-//your JS code here. If required.
-
 const name = document.getElementById("name");
 const age = document.getElementById("age");
 const button = document.getElementById("btn");
-
+let nameVal,ageVal
 name.addEventListener("input",(e)=>{
-	const nameVal = e.target.value;
+	nameVal = e.target.value;
 })
 age.addEventListener("input",(e)=>{
-	const ageVal = e.target.value;
+	 ageVal = e.target.value;
 })
-button.addEventListener("click",()=>{
-	
+button.addEventListener("click",(e)=>{
+	e.preventDefault();
+	promise(nameVal,ageVal)
+		.then((data) => alert(data))
+		.catch((err) => alert(err))
 })
 
-function 
+function promise(name, age) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (age > 18) {
+        resolve(`Welcome, ${name}. You can vote.`);
+      } else {
+        reject(`Oh sorry ${name}. You aren't old enough.`);
+      }
+    }, 4000);
+  });
+}
+
+
 
 
 
